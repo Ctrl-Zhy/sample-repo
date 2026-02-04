d1a15b1a844f873dbab8a72c1e243c7e017591a6
Ctrl-Zhy <73355854+Ctrl-Zhy@users.noreply.github.com>
2026-02-05 01:49:20 +0800

    Add basic Java Main class

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Basic Main class for Ctrl-Zhy/sample-repo.
+ */
+public class Main {
+    /**
+     * Entry point.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello from Ctrl-Zhy/sample-repo!");
+    }
+}
